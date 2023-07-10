363c7531f7482d1c020204432051f630cc10c56e
Evaristo Ferreira <evaristo.ferreira@propig.com.br>
2023-07-10 17:33:04 -0300

    add app funcionarios

---
+++ apps/funcionarios/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+from apps.departamentos.models import Departamentos
+from apps.empresas.models import Empresa
+
+
+class Funcionario(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100)
+
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    departamentos = models.ManyToManyField(Departamentos)
+    empresa = models.ForeignKey(Empresa, null=True, blank=True, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.nome
