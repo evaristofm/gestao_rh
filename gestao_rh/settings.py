d43deb34b7041f39b437bd74eae7554acf1108fc
Evaristo Ferreira <evaristo.ferreira@propig.com.br>
2023-07-12 17:06:32 -0300

    add requirements and django-bootstrato-form

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrapform',
     'apps.empresas',
     'apps.funcionarios',
     'apps.departamentos',
